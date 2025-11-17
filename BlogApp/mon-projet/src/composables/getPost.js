a8716458c33b4976b94c88daad90a28a980ec5ac
Fatima Chegdani <chegdanifatima82@gmail.com>
2025-11-17 22:12:14 +0100

    Suppression du sous-dossier BlogApp/mon-projet

--- BlogApp/mon-projet/src/composables/getPost.js
+++
@@ -1,8 +0,0 @@
-export function getPost(id) {
-  return fetch(`http://localhost:3000/posts/${id}`)
-    .then(res => res.json())
-    .catch(err => {
-      console.log("Erreur :", err);
-      return null; // un seul post, pas un tableau
-    });
-}
--- BlogApp/mon-projet/src/composables/getPosts.js
+++
@@ -1,8 +0,0 @@
-export function getPosts() {
-  return fetch("http://localhost:3000/posts")
-    .then(res => res.json())
-    .catch(err => {
-      console.log("Erreur :", err);
-      return []; 
-    });
-}
